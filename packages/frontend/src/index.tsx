e3d7f81f3b5bbddf1d47a716863ce1b9e170debd
Priyanshu Dangare <aspizu@protonmail.com>
2026-02-03 01:31:33 +0530

    fix: add DOM error handling patches to global index

--- packages/frontend/src/index.tsx
+++ packages/frontend/src/index.tsx
@@ -2,11 +2,39 @@ import "@/styles/global.css"
 import {StrictMode} from "react"
 import {createRoot} from "react-dom/client"
 
-// @ts-ignore
+// @ts-expect-error foo
 import "@fontsource-variable/figtree"
-// @ts-ignore
+// @ts-expect-error foo
 import "@fontsource-variable/fira-code"
 import App from "./features/app"
+
+if (typeof Node === "function" && Node.prototype) {
+    const originalRemoveChild = Node.prototype.removeChild
+    // @ts-expect-error foo
+    Node.prototype.removeChild = function (child) {
+        try {
+            // @ts-expect-error foo
+            // eslint-disable-next-line prefer-rest-params
+            return originalRemoveChild.apply(this, arguments)
+        } catch (e) {
+            console.error("Error in removeChild:", e, child, this)
+        }
+    }
+}
+
+if (typeof Node === "function" && Node.prototype) {
+    const originalAppendChild = Node.prototype.appendChild
+    // @ts-expect-error foo
+    Node.prototype.appendChild = function (child) {
+        try {
+            // @ts-expect-error foo
+            // eslint-disable-next-line prefer-rest-params
+            return originalAppendChild.apply(this, arguments)
+        } catch (e) {
+            console.error("Error in appendChild", e, child, this)
+        }
+    }
+}
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
