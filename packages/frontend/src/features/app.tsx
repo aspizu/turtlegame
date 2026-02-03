ecf53eac4e90ed5fca857cefb59c865a151ba3b8
Priyanshu Dangare <aspizu@protonmail.com>
2026-02-03 17:29:02 +0530

    feat: add offline indicator

--- packages/frontend/src/features/app.tsx
+++ packages/frontend/src/features/app.tsx
@@ -1,9 +1,11 @@
+import {Spinner} from "@/components/ui/spinner"
+import {cn} from "@/lib/utils"
 import Game from "@/pages/game"
 import Home from "@/pages/home"
 import WaitingRoom from "@/pages/waiting-room"
 import socket from "@/services/socket"
 import {useAppStore} from "@/stores/app-store"
-import {useEffect} from "react"
+import {useEffect, useState} from "react"
 
 export default function App() {
     const view = useAppStore((state) => state.view.view)
@@ -25,12 +27,36 @@ export default function App() {
             listener.off()
         }
     }, [addMessage])
-    switch (view) {
-        case "menu":
-            return <Home />
-        case "waiting-room":
-            return <WaitingRoom />
-        case "in-game":
-            return <Game />
-    }
+    const [online, setOnline] = useState(false)
+    useEffect(() => {
+        const listener1 = socket.on("connect", () => {
+            setOnline(true)
+        })
+        const listener2 = socket.on("disconnect", () => {
+            setOnline(false)
+        })
+        const listener3 = socket.on("connect_error", () => {
+            setOnline(false)
+        })
+        return () => {
+            listener1.off()
+            listener2.off()
+            listener3.off()
+        }
+    }, [])
+    return (
+        <>
+            <div
+                className={cn(
+                    "bg-destructive fixed top-0 right-0 flex items-center justify-center gap-2 px-2 py-1 text-lg font-medium transition-all",
+                    online ? "opacity-0" : "opacity-100",
+                )}
+            >
+                OFFLINE <Spinner className="size-6" />
+            </div>
+            {view === "menu" && <Home />}
+            {view === "waiting-room" && <WaitingRoom />}
+            {view === "in-game" && <Game />}
+        </>
+    )
 }
