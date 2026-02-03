6c95c882e5eaa80924b52b3ffc71e60b06e4d3e3
Priyanshu Dangare <aspizu@protonmail.com>
2026-02-03 07:45:50 +0530

    fix: ensure websocket transport is explicitly set for socket connection

--- packages/frontend/src/services/socket.ts
+++ packages/frontend/src/services/socket.ts
@@ -5,5 +5,8 @@ import type {
 } from "../../../backend/src/protocol.ts"
 const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io(
     import.meta.env.VITE_SOCKETURL,
+    {
+        transports: ["websocket"],
+    },
 )
 export default socket
