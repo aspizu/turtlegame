0f9658da1a028cf5281ae67d77d40652a8918098
Priyanshu Dangare <aspizu@protonmail.com>
2026-02-03 07:08:33 +0530

    refactor: reorder imports in index.tsx

--- packages/frontend/src/index.tsx
+++ packages/frontend/src/index.tsx
@@ -1,12 +1,12 @@
 import "@/styles/global.css"
 import {StrictMode} from "react"
 import {createRoot} from "react-dom/client"
+import App from "./features/app"
 
 // @ts-expect-error foo
 import "@fontsource-variable/figtree"
 // @ts-expect-error foo
 import "@fontsource-variable/fira-code"
-import App from "./features/app"
 
 if (typeof Node === "function" && Node.prototype) {
     const originalRemoveChild = Node.prototype.removeChild
